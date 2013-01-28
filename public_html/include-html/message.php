8b628145b9068bae63e49ddc1b846fecf249b0cb
Matthew Spraggs <matthew@matthew-PBL21.(none)>
2013-01-28 20:51:07 +0000

    Added php script to add offer to the database

---
+++ public_html/include-html/message.php
@@ -0,0 +1,7 @@
+<?php
+function error($message) {
+?>
+  <p><?php echo $message; ?></p>
+<?php
+}
+?>
--- public_html/include-php/offer.php
+++ public_html/include-php/offer.php
@@ -1,10 +1,36 @@
-<div class="form">
-   <form action="index.php" method="post" name="register">
-   <p>Offer: </p>
-   <input type="text" name="offer" />
-   <br />
-   <p>Email (optional):</p>
-   <input type="text" name="email" />
-   <br/><br />
-   <input type="submit" value="Submit" />
-</div>
+<?php
+include("include-html/message.php");
+
+//Check to see if the user is trying to 
+if(!isset($_POST['offer'])) {
+  include("include-html/offer.html");
+}
+else {
+  if(filter_var($_POST['offer'],FILTER_VALIDATE_INT)) {
+    //Grab the numerical value from the post data.
+    $offer = filter_var($_POST['offer'],FILTER_SANITIZE_NUMBER_INT);
+    if($offer <= 100 && $offer >= 0) {
+      sql_connect("ultimatumgame");
+      if($_POST['email'] != "" && filter_var($_POST['email'],FILTER_VALIDATE_EMAIL)) {
+	$email = filter_var($_POST['email'],FILTER_SANITIZE_EMAIL);
+	$query = "insert into game1 (id,player,offer,email,contact) values (0, 1, '$offer', '$email', 'Y')";
+      }
+      else {
+	$query = "insert into game1 (id,player,offer,contact) values (0, 1, '$offer', 'N')";
+      }
+      mysql_query($query) or die();
+      message("Thank you for your participation");
+      //
+    }
+    else {
+      //If the number isn't between 0 and 100, we've got a problem
+      include("include-html/offer.html");
+      message("Please enter a number between 0 and 100.")
+    }
+  }
+  else {
+    //The input must be numeric, otherwise we'll run into trouble
+    include("include-html/offer.html");
+    message("Please enter a number between 0 and 100.");
+  }
+}
