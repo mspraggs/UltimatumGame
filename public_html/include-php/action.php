41ee12f0523c3ca19e425c93b14cc4179dd2520c
Matthew Spraggs <matthew@matthew-PBL21.(none)>
2013-01-28 19:31:06 +0000

    Adjusted those relative paths so they point to the right folders

--- public_html/include-php/action.php
+++ public_html/include-php/action.php
@@ -9,13 +9,13 @@ else {
     include("include-php/game1.php");
   }
   elseif($_GET['action']=="respond") {
-    include("../include-php/respond.php");
+    include("include-php/respond.php");
   }
   elseif($_GET['action']=="offer") {
-    include("../include-php/offer.php");
+    include("include-php/offer.php");
   }
   else {
-    include("../include-html/index.html");
+    include("include-html/index.html");
   }
 }
 ?>
