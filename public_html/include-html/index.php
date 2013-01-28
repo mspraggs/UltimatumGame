3dfdbfa26b872b198bfa00892d597d63b4a3d1c7
Matthew Spraggs <matthew@matthew-PBL21.(none)>
2013-01-28 18:29:45 +0000

    Shuffled offer.php into include-php directory, renamed index.php to index.html and added handling for respond and offer actions

--- public_html/include-html/index.php
+++
@@ -1,5 +0,0 @@
-<div class="form">
-   <form action="index.php?action=game1">
-   <input type="submit" value="Play the Game"/>
-   </form>
-</div>
--- public_html/include-php/action.php
+++ public_html/include-php/action.php
@@ -1,18 +1,21 @@
 <?php
-if(!isset($_GET['action']))
-  {
-    //if it's not set, display the homepage.
-    include("include-html/index.php");
+if(!isset($_GET['action'])) {
+  //if it's not set, display the homepage.
+  include("include-html/index.php");
+}
+else {
+ //Else figure out which page to include
+  if($_GET['action']=="play") {
+    include("include-php/game1.php");
   }
-else
-  { //Else figure out which page to include
-    if($_GET['action']=="game1")
-      {
-	include("include-php/game1.php");
-      }
-    else
-      {
-	include("include-html/index.php");
-      }
+  elseif($_GET['action']=="respond") {
+    include("include-php/respond.php");
   }
+  elseif($_GET['action']=="offer") {
+    include("include-php/offer.php");
+  }
+  else {
+    include("include-html/index.html");
+  }
+}
 ?>
