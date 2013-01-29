12c307883d082bf31a4dc24d025bfe7c94d4993a
Matt <matthew.spraggs@gmail.com>
2013-01-29 11:35:17 +0000

    Testing game1.php type selection.

--- public_html/include-php/game1.php
+++ public_html/include-php/game1.php
@@ -6,9 +6,11 @@ sql_connect("ultimatumgame");
 $query = "select offer where result=''";
 
 $results = mysql_query($query);
+$a=rand(0,1);
 
-if(mysql_num_rows($results) == 0 || rand(0,1) == 0) {
-  header("Location: index.php?action=offer");
+if(mysql_num_rows($results) == 0 || $a == 0) {
+  echo mysql_num_rows($results)."<br />".$a;
+  //header("Location: index.php?action=offer");
 }
 else {
   header("Location: index.php?action=respond");
