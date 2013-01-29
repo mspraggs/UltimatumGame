db28440d48adbba211025fb479e9f2d29e8a78e9
Matt <matthew.spraggs@gmail.com>
2013-01-29 11:40:26 +0000

    Still testing the game selection script, which isn't playing ball for some reason

--- public_html/include-php/game1.php
+++ public_html/include-php/game1.php
@@ -9,7 +9,7 @@ $results = mysql_query($query);
 $a=rand(0,1);
 
 if(mysql_num_rows($results) == 0 || $a == 0) {
-  echo mysql_num_rows($results)."<br />".$a;
+  echo "<br />".mysql_num_rows($results)."<br />".$a;
   //header("Location: index.php?action=offer");
 }
 else {
