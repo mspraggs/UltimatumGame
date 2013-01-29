693afa2d988a5a9e9e09d16295ac85ce54b0195c
Matthew Spraggs <matthew@matthew-PBL21.(none)>
2013-01-29 15:15:45 +0000

    Forgot to close mysql session when selecting game

--- public_html/include-php/game1.php
+++ public_html/include-php/game1.php
@@ -2,9 +2,9 @@
 include_once("../protected-324hjk/sql_connect.php");
 
 sql_connect("ultimatumgame");
-
 $query = "select offer from game1 where result is null";
 $results = mysql_query($query);
+mysql_close();
 
 if(mysql_num_rows($results) == 0 || rand(0,1) == 0) {
   header("Location: index.php?action=offer");
