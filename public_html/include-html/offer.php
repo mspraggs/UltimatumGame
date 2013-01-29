75b437e00373acf55f051103823059c17aaf3008
Matthew Spraggs <matthew@matthew-PBL21.(none)>
2013-01-29 16:17:26 +0000

    Added some blurb to the offer page.

--- public_html/include-html/offer.php
+++ public_html/include-html/offer.php
@@ -2,6 +2,8 @@
 function offer_form($csrf) {
 ?>
 <div class="form">
+    <p>Imagine you have a pile of one hundred pound coins, dollar coins, or other currency of your choosing. You are going to offer any number of these coins (including 0 or 100) to the second player in this game. If they accept your offer, you both recieve the coins as allocated by yourself. If they don't, neither of you get any coins. Please choose the number of coins you'd like to offer and click submit. It is important that you base your decision on what you'd do if this scenario was real.</p>
+    <p>You may include your email address if you want to find out whether your offer is accepted. Your email will be used for no purpose other than to notify you of whether your offer is accepted of rejected by the other player.</p>
    <form action="index.php?action=offer&csrf=<?php echo $csrf; ?>" method="post">
      <p>Offer: </p>
      <input type="text" name="offer" />
