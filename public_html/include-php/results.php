f8cf8a62ba6cdbf8f1b6c089d2576c43b2531bfb
Matthew Spraggs <matthew@matthew-PBL21.(none)>
2013-01-29 15:10:43 +0000

    Referred to wrong table in query

--- public_html/include-php/results.php
+++ public_html/include-php/results.php
@@ -4,7 +4,7 @@ include_once("../protected-324hjk/sql_connect.php");
 include_once("include-html/results.php");
 
 sql_connect("ultimatumgame");
-$query = "select player,offer,result from game where result is not null";
+$query = "select player,offer,result from game1 where result is not null";
 $results = mysql_query($query);
 mysql_close();
 
