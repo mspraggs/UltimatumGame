4c9e9c70ff617c0cd4328dbf06512d55bc9272c8
Matt <matthew.spraggs@gmail.com>
2013-01-29 12:14:27 +0000

    Wasn't using the correct syntax for query (didn't refer to table). Should be fixed now.

--- public_html/include-php/game1.php
+++ public_html/include-php/game1.php
@@ -3,16 +3,11 @@ include_once("../protected-324hjk/sql_connect.php");
 
 sql_connect("ultimatumgame");
 
-$query = "select offer where result is null";
-
+$query = "select offer from game1 where result is null";
 $results = mysql_query($query);
-$a = rand(0,1);
-$b = mysql_num_rows($results);
-echo $b;
 
-if(0 == 0 || $a == 0) {
-  //echo "<br />".$b."<br />".$a;
-  //header("Location: index.php?action=offer");
+if(mysql_num_rows($results) == 0 || rand(0,1) == 0) {
+  header("Location: index.php?action=offer");
 }
 else {
   header("Location: index.php?action=respond");
