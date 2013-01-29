622d2258141cffce8b067f05e1d53e49685ba6f0
Matthew Spraggs <matthew@matthew-PBL21.(none)>
2013-01-29 16:31:18 +0000

    Changed the blurb on the homepage

--- public_html/include-html/respond.php
+++ public_html/include-html/respond.php
@@ -2,9 +2,10 @@
 
 function response_form($offer,$offer_id, $csrf) {
 ?>
+<p>You have been offered <?php echo $offer; ?> coins (pounds, dollars, or other currency of your choosing) by the first player in this game, out of a total of one hundred coins. If you accept their offer, you will receive <?php echo $offer; ?> coins and the first player will receive the remainder from the original pile of one hundred. If you refuse the offer, neither of you will receive anything. Please decide whether to accept the offer or not. Remember that you should choose based on what you'd do if this scenario was real.</p>
+
 <div class="form">
-    <p>You have been offered <?php echo $offer; ?> coins (pounds, dollars, or other currency of your choosing) by the first player in this game, out of a total of one hundred coins. If you accept their offer, you will receive <?php echo $offer; ?> coins and the first player will receive the remainder from the original pile of one hundred. If you refuse the offer, neither of you will receive anything. Please decide whether to accept the offer or not. Remember that you should choose based on what you'd do if this scenario was real.</p>
-  <form action="index.php?action=respond&csrf=<?php echo $csrf; ?>" method="post">
+    <form action="index.php?action=respond&csrf=<?php echo $csrf; ?>" method="post">
     <input type="submit" value="Accept" />
     <input type="hidden" name="result" value="accept" />
     <input type="hidden" name="id" value="<?php echo $offer_id; ?>" />
