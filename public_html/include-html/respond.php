106f3703ec9839b37dfabfc3e97061f48eff1e2a
Matt <matthew.spraggs@gmail.com>
2013-01-29 14:04:40 +0000

    Fixed another link in the respond form

--- public_html/include-html/respond.php
+++ public_html/include-html/respond.php
@@ -10,7 +10,7 @@ function response_form($offer,$offer_id) {
   </form>
   <br />
   <br />
-  <form action="index.php?action=response" method="post">
+  <form action="index.php?action=respond" method="post">
     <input type="submit" value="Reject" />
     <input type="hidden" name="result" value="reject" />
     <input type="hidden" name="id" value="<?php echo $offer_id; ?>" />
