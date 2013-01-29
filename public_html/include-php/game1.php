75d9804cd828c334becf24f977b0c68f9428ede6
Matthew Spraggs <matthew@matthew-PBL21.(none)>
2013-01-29 15:57:24 +0000

    Added cookie creation to respond.php and querying to game1.php

--- public_html/include-php/game1.php
+++ public_html/include-php/game1.php
@@ -1,19 +1,25 @@
 <?php
 include_once("../protected-324hjk/sql_connect.php");
 
-sql_connect("ultimatumgame");
-$query = "select offer from game1 where result is null";
-$results = mysql_query($query);
-mysql_close();
+if(!isset($_COOKIE['participated'])) {
 
-//Create a csrf token to prevent people randomly adding results to the database.
-$_SESSION['token'] = md5(uniqid(mt_rand(),true));
+  sql_connect("ultimatumgame");
+  $query = "select offer from game1 where result is null";
+  $results = mysql_query($query);
+  mysql_close();
 
-if(mysql_num_rows($results) == 0 || rand(0,1) == 0) {
-  header("Location: index.php?action=offer&csrf=".$_SESSION['token']);
+  //Create a csrf token to prevent people randomly adding results to the database.
+  $_SESSION['token'] = md5(uniqid(mt_rand(),true));
+
+  if(mysql_num_rows($results) == 0 || rand(0,1) == 0) {
+    header("Location: index.php?action=offer&csrf=".$_SESSION['token']);
+  }
+  else {
+    header("Location: index.php?action=respond&csrf=".$_SESSION['token']);
+  }
 }
 else {
-  header("Location: index.php?action=respond&csrf=".$_SESSION['token']);
+  include_once("include-html/message.php");
+  message("It looks like you've already participated. Thank you!")
 }
-
 ?>
