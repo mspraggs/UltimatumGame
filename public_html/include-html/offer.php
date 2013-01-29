5c88fa5021e719891137fcb7bd2f464d35836d88
Matthew Spraggs <matthew@matthew-PBL21.(none)>
2013-01-29 17:13:44 +0000

    Updated the offer blurb to include a disclaimer about emails etc.

--- public_html/include-html/offer.php
+++ public_html/include-html/offer.php
@@ -13,6 +13,7 @@ function offer_form($csrf) {
      <input type="submit" value="Submit" />
    </form>
 </div>
+<p>Note: your email will be used solely to let you know whether your offer is accepted or not. It will not be disseminated to anyone and will be removed from the website database as soon as you are notified of the acceptance or refusal of your offer.</p>
 <?php
 }
 ?>
