10f4873dbdd6d4fe165fd3a2fc1390f8961fb1de
Matt Spraggs <matthew.spraggs@gmail.com>
2013-01-29 22:49:45 +0000

    Added some pagination html stuff

--- public_html/include-php/results.php
+++ public_html/include-php/results.php
@@ -3,8 +3,34 @@
 include_once("../protected-324hjk/sql_connect.php");
 include_once("include-html/results.php");
 
+//Check if we're using pagination
+if(!(isset($_GET['pagenum']))) {
+  //If not set, set the pagenumber to 1
+  $pagenum = 1;
+}
+else {
+  //Else get it from the GET variables
+  $pagenum=filter_var($_GET['pagenum'],FILTER_SANITIZE_NUMBER_INT);
+}
+
 sql_connect("ultimatumgame");
 $query = "select player,offer,result from game1 where result is not null";
+$results = mysql_query($query);
+
+//Get number of blog entries
+$rows=mysql_num_rows($results);
+	
+//How many posts per page?
+$page_rows=10;
+//Calculate the last page number
+$last=ceil($rows/$page_rows);
+//Sanity check the pagenumber
+if($pagenum<1) $pagenum=1;
+elseif($pagenum>$last) $pagenum=$last;
+//Construct the pagination query filter
+$max='LIMIT '.($pagenum-1)*$page_rows.','.$page_rows;
+
+$query="select player, offer, result from game1 where result is not null $max"
 $results = mysql_query($query);
 mysql_close();
 
