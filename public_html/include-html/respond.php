754be7ecbd1c2c8bc55a2cabc359fecdf7d6deef
Matthew Spraggs <matthew@matthew-PBL21.(none)>
2013-01-29 15:32:45 +0000

    Added reference to csrf in form action so that it's passed back to the script correctly

--- public_html/include-html/respond.php
+++ public_html/include-html/respond.php
@@ -1,16 +1,16 @@
 <?php
 
-function response_form($offer,$offer_id) {
+function response_form($offer,$offer_id, $csrf) {
 ?>
 <div class="form">
-  <form action="index.php?action=respond" method="post">
+  <form action="index.php?action=respond&csrf=<?php echo $csrf; ?>" method="post">
     <input type="submit" value="Accept" />
     <input type="hidden" name="result" value="accept" />
     <input type="hidden" name="id" value="<?php echo $offer_id; ?>" />
   </form>
   <br />
   <br />
-  <form action="index.php?action=respond" method="post">
+  <form action="index.php?action=respond&csrf=<?php echo $csrf; ?>" method="post">
     <input type="submit" value="Reject" />
     <input type="hidden" name="result" value="reject" />
     <input type="hidden" name="id" value="<?php echo $offer_id; ?>" />
