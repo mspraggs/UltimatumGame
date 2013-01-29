3121ae7aafd5ef92646aaa5adc38bff7d33d459d
Matthew Spraggs <matthew@matthew-PBL21.(none)>
2013-01-29 15:19:39 +0000

    Added creation of csrf token to game1.php

--- public_html/include-php/game1.php
+++ public_html/include-php/game1.php
@@ -6,11 +6,14 @@ $query = "select offer from game1 where result is null";
 $results = mysql_query($query);
 mysql_close();
 
+//Create a csrf token to prevent people randomly adding results to the database.
+$_SESSION['token'] = md5(uniqid(mt_rand(),true));
+
 if(mysql_num_rows($results) == 0 || rand(0,1) == 0) {
-  header("Location: index.php?action=offer");
+  header("Location: index.php?action=offer&csrf=".$_SESSION['token']);
 }
 else {
-  header("Location: index.php?action=respond");
+  header("Location: index.php?action=respond&csrf=".$_SESSION['token']);
 }
 
 ?>
