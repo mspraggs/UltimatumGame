75d248b54f8cf700f5ad388c567c6d25a17884dc
Matthew Spraggs <matthew@matthew-PBL21.(none)>
2013-01-29 16:23:07 +0000

    Added some blurb to the respond.php html file

--- public_html/include-html/respond.php
+++ public_html/include-html/respond.php
@@ -3,6 +3,7 @@
 function response_form($offer,$offer_id, $csrf) {
 ?>
 <div class="form">
+    <p>You have been offered <?php echo $offer; ?> coins (pounds, dollars, or other currency of your choosing) by the first player in this game, out of a total of one hundred coins. If you accept their offer, you will receive <?php echo $offer; ?> coins and the first player will receive the remainder from the original pile of one hundred. If you refuse the offer, neither of you will receive anything. Please decide whether to accept the offer or not. Remember that you should choose based on what you'd do if this scenario was real.</p>
   <form action="index.php?action=respond&csrf=<?php echo $csrf; ?>" method="post">
     <input type="submit" value="Accept" />
     <input type="hidden" name="result" value="accept" />
